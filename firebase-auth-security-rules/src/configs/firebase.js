b106875e0fe018b8809d2557fdf07962f0a6accf
Cecilie94 <144773754+Cecilie94@users.noreply.github.com>
2024-04-03 07:56:19 +0200

    firebase authentication

---
+++ firebase-auth-security-rules/src/configs/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvPHeaBVz9Tfpg3d_IMkuE0QfQH8m2pvo",
+  authDomain: "firestore-test-9bf50.firebaseapp.com",
+  projectId: "firestore-test-9bf50",
+  storageBucket: "firestore-test-9bf50.appspot.com",
+  messagingSenderId: "1022747025090",
+  appId: "1:1022747025090:web:8ff8577a0e7a86a532255b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ firebase-auth-security-rules/src/views/SigninView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+import { onSignOut, stateChange } from "@/services/AuthService";
+import SigninForm from "@/components/SigninForm.vue";
+
+const user = ref(null);
+
+stateChange((userData) => {
+  user.value = userData;
+});
+</script>
+
+<template>
+  <SigninForm v-if="!user" />
+  <button @click="onSignOut()" v-else>Sign Out</button>
+</template>
+
+<style scoped></style>
